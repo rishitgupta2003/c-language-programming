bf48ac8047855ea44612dd4495aa6cd3714b3dd3
rishitgupta203 <rishitgupta203@gmail.com>
2025-06-23 21:37:03 +0530

    DMA Basics Done

--- DynamicMemoryAllocation/DMA.c
+++ DynamicMemoryAllocation/DMA.c
@@ -5,32 +5,49 @@
 
 int main(){
     //malloc & realloc example    
-    char *str = NULL;
+    // char *str = NULL;
 
-    str = (char*) malloc(7*sizeof(char));
-    strcpy(str, "Rishit");
+    // str = (char*) malloc(7*sizeof(char));
+    // strcpy(str, "Rishit");
 
-    char* ptr = str;
+    // char* ptr = str;
 
-    printf("String = > %s\t\tAddress => %p\n\n", str, str);
+    // printf("String = > %s\t\tAddress => %p\n\n", str, str);
     
-    while(*ptr){
-        printf("Character => %c\n", *ptr);
-        ptr++;
+    // while(*ptr){
+    //     printf("Character => %c\n", *ptr);
+    //     ptr++;
+    // }
+
+    // str = (char*) realloc(str, 14*sizeof(char));
+
+    // strcat(str, " Gupta");
+
+    // printf("Update String => %s\tAddress => %p\n\n", str, str);
+
+    // ptr = str;
+    // while(*ptr){
+    //     printf("Character => %c\n", *ptr);
+    //     ptr++; 
+    // }
+
+    // free(str);
+
+    int size;
+    printf("Enter Size of String: ");
+    scanf("%d",&size);
+    getchar();
+    
+    char* ptr = malloc(size * sizeof(char));
+
+    if(ptr != NULL){
+        printf("Enter String: ");
+        fgets(ptr, size, stdin);
     }
+    printf("String entered by User => %s\n", ptr);
 
-    str = (char*) realloc(str, 14*sizeof(char));
-
-    strcat(str, " Gupta");
-
-    printf("Update String => %s\tAddress => %p\n\n", str, str);
-
-    ptr = str;
-    while(*ptr){
-        printf("Character => %c\n", *ptr);
-        ptr++; 
-    }
-
-    free(str);
+    free(ptr);
+    ptr = NULL;
+    return 0;
     
 }
