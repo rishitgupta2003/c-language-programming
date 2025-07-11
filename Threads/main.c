8eeba187a942d139634006dd08268c0fff4cb560
rishitgupta2003 <rishitgupta203@gmail.com>
2025-07-12 01:31:05 +0530

    Threads Creation Joining Exit Done

--- Threads/main.c
+++ Threads/main.c
@@ -16,33 +16,51 @@
 #include<stdio.h>
 #include<pthread.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 struct Data {
     int multiplier;
     int acc_bal;
 };
 
+int shared_variable = 0;
+
 void* threadHello(void* arg);
 void* final_amount(void* arg);
+void* check_for_race(void* arg);
 
 int main(int argc, char* argv[]) {
+    // pthread_t thread;
+    //
+    // struct Data* data = (struct Data*) malloc(sizeof(struct Data));
+    //
+    // data->acc_bal = atoi(argv[1]);
+    // data->multiplier = atoi(argv[2]);
+    //
+    // pthread_create(&thread, NULL, final_amount, (void*) data);
+    //
+    // int* save_amount = NULL;
+    //
+    // pthread_join(thread, (void**) &save_amount);
+    //
+    // printf("Final Calculated Amount: %d\n", *save_amount);
+    // free(save_amount);
+    // free(data);
+    // pthread_exit(NULL);
+
     pthread_t thread;
+    pthread_t thread2;
+    pthread_t thread3;
 
-    struct Data* data = (struct Data*) malloc(sizeof(struct Data));
+    pthread_create(&thread, NULL, check_for_race, NULL);
+    pthread_create(&thread2, NULL, check_for_race, NULL);
+    pthread_create(&thread3, NULL, check_for_race, NULL);
 
-    data->acc_bal = atoi(argv[1]);
-    data->multiplier = atoi(argv[2]);
+    pthread_join(thread, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
 
-    pthread_create(&thread, NULL, final_amount, (void*) data);
-
-    int* save_amount = NULL;
-
-    pthread_join(thread, (void**) &save_amount);
-
-    printf("Final Calculated Amount: %d\n", *save_amount);
-    free(save_amount);
-    free(data);
-    pthread_exit(NULL);
+    printf("Final Value of shared Value: %d\n", shared_variable);
 }
 
 void* threadHello(void* arg) {
@@ -56,4 +74,16 @@ void* final_amount(void *arg) {
     *result = data->multiplier * data->acc_bal;
 
     return result;
+}
+
+void * check_for_race(void *arg) {
+    for (int i = 0; i < 10; i++) {
+        int temp = shared_variable;
+        usleep(1);
+        temp++;
+        shared_variable = temp;
+        printf("Shared Variable: %d\n", shared_variable);
+    }
+
+    return NULL;
 }
